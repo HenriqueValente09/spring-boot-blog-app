b6c14a2ef3397578035af4cb5b342406f53c545e
HenriqueValente09 <valentedev09@gmail.com>
2023-09-20 10:39:19 -0300

    feat: account service

---
+++ src/main/java/com/example/springbootblogapp/services/AccountService.java
@@ -0,0 +1,19 @@
+package com.example.springbootblogapp.services;
+
+import com.example.springbootblogapp.models.Account;
+import com.example.springbootblogapp.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account save(Account account) {
+        return accountRepository.save(account);
+    }
+}
